e3594bc864811f3c518057f950fad5503b32c066
adamknapp124 <aknapp124@gmail.com>
2023-11-13 22:27:55 -0800

    Added avatar component

--- app/components/sidebar/DesktopSidebar.js
+++ app/components/sidebar/DesktopSidebar.js
@@ -2,11 +2,15 @@
 
 import { useState } from 'react';
 import useRoutes from '../../hooks/useRoutes';
+
+import Avatar from '../Avatar';
 import DesktopItem from './DesktopItem';
 
-const DesktopSidebar = () => {
+const DesktopSidebar = ({ currentUser }) => {
 	const routes = useRoutes();
 	const [isOpen, setIsOpen] = useState(false);
+
+	console.log('current user: ', currentUser);
 
 	return (
 		<div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
@@ -23,6 +27,13 @@ const DesktopSidebar = () => {
 						/>
 					))}
 				</ul>
+			</nav>
+			<nav className="mt-4 flex flex-col justify-between items-center">
+				<div
+					onClick={() => setIsOpen(true)}
+					className="cursor-pointer hover:opacity-75 transition">
+					<Avatar user={currentUser} />
+				</div>
 			</nav>
 		</div>
 	);
--- app/components/sidebar/Sidebar.js
+++ app/components/sidebar/Sidebar.js
@@ -1,10 +1,13 @@
 import DesktopSidebar from './DesktopSidebar';
 import MobileFooter from './MobileFooter';
+import getCurrentUser from '../../actions/getCurrentUser';
 
 async function Sidebar({ children }) {
+	const currentUser = await getCurrentUser();
+
 	return (
 		<div className="h-full">
-			<DesktopSidebar />
+			<DesktopSidebar currentUser={currentUser} />
 			<MobileFooter />
 			<main className="lg:pl-20 h-full">{children}</main>
 		</div>

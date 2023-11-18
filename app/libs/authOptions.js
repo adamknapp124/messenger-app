5c6b914ec9a0fc9a069de7c26d512c61b5c11bc1
adamknapp124 <aknapp124@gmail.com>
2023-11-18 15:10:09 -0800

    changed import for prisma

--- app/libs/authOptions.js
+++ app/libs/authOptions.js
@@ -3,7 +3,7 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import prisma from '../../../libs/prismadb';
+import prisma from './prismadb';
 
 export const authOptions = {
 	adapter: PrismaAdapter(prisma),
